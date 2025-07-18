c80a137cf3d2ca376b396a3427b92b58ebdc5aed
JustinGamboa <gamboajustin03@gmail.com>
2025-07-17 21:39:54 -0400

    i dont know

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -36,7 +36,7 @@ const app = express();
 //Define Middleware
 app.use(cors());
 app.use(express.json());
-app.options("\\*", cors());
+
 //Mount Routes
 app.use("/api/users", userRoutes);
 app.use("/api/news", newsRoutes);
@@ -45,7 +45,7 @@ app.use("/api/team", teamRoutes);
 //Pointing Express to Build directory
 const buildPath = join(__dirname, "..", "Frontend", "dist"); 
 app.use(express.static(buildPath));
-app.get("*", (_req, res) => res.sendFile(join(buildPath, "index.html")));
+app.get("\\*", (_req, res) => res.sendFile(join(buildPath, "index.html")));
 
 // main -- server functionality
 async function main() {
