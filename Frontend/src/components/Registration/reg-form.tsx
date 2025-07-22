31883fe53767852f576cce028f5117e9d1f06ad1
GiorgioMiguel <160094651+GiorgioMiguel@users.noreply.github.com>
2025-07-21 23:05:13 -0400

    STABLE VERSION REMOVED GOOGLE LOG IN AND CONTACT US BUTTONS

--- Frontend/src/components/Registration/reg-form.tsx
+++ Frontend/src/components/Registration/reg-form.tsx
@@ -162,15 +162,6 @@ export function RegisterForm({
           <Button type="submit" className="w-full">
             Register
           </Button>
-          <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
-            <span className="bg-background text-muted-foreground relative z-10 px-2">
-              Or continue with
-            </span>
-          </div>
-          <Button variant="outline" className="w-full">
-            <img src="/google.svg" alt="Google logo" width={24} height={24} />
-            Register with Google
-          </Button>
         </div>
         <div className="text-center text-sm">
           Already have an account?{" "}
